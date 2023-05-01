81479ceb0e51b77dcdea85f090edbe8952586a11
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-05-01 14:31:29 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -13,6 +15,29 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find(singleNews => singleNews._id === id)
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    // console.log(id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoriesNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoriesNews);
+    }
 })
 
 app.listen(port, () => {
